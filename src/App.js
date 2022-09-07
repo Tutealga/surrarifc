db7bac9759ec1360539843b9000950b235e914f1
Mateo Alganaras <tutealga@gmail.com>
2022-09-07 20:15:40 -0300

    Arreglos Readme

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Footer from './components/Footer/Footer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Plantilla from './components/Plantilla/Plantilla';
 
 const App = () => {
@@ -17,6 +17,7 @@ const App = () => {
    <Route path='/categoria/:id' element={<ItemListContainer />}/>
    <Route path='/detalle/:id' element={<ItemDetailContainer />}/>
    <Route path='/plantilla' element={<Plantilla />}/> 
+   <Route path='*' element={<Navigate to={"/"}/>}/>
    </Routes>
   <Footer />
    </BrowserRouter> 
