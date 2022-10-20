76b1a2f64e928ff344e0821662222f30613ab41e
Mateo Alganaras <tutealga@gmail.com>
2022-10-20 16:39:43 -0300

    Padding container 'gracias por su compra'

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Order = ({orderId}) => {
     return(
         <section className=''>
-            <Container className="container-sm container-fluid my-5">
+            <Container className="container-fluid p-5 my-5">
                 <h1 class="text-center">¡Gracias por su compra!</h1>
-                <p className="px-3">Su número de orden es: <span className='fw-bold text-warning'>{orderId}</span></p>
+                <p className="px-3">Su número de orden es: <span className='fw-bold text-danger'>{orderId}</span></p>
                 <p className="px-3">Estará recibiendo su pedido dentro de los próximos 5 días hábiles.</p>
                 <Link id="goBuy" to='/'>Volver al inicio</Link>
             </Container>
